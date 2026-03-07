d867d22bf583b2bae6eb01bd09781450629b8bff
cam_s <tinchantcamille@outlook.com>
2026-03-07 20:47:44 +0100

    Create clean simple Metro config without withNativeWind

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,5 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-const path = require("path");
 
-const config = getDefaultConfig(__dirname, {
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
-});
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, {
-  input: "./global.css",
-  forceWriteFileSystem: true,
-});
+module.exports = config;
