7e05a100ceb7f93cf02268db98f4a24f0c701760
cam_s <tinchantcamille@outlook.com>
2026-03-07 20:37:59 +0100

    Simplify Metro config to avoid configs.toReversed error

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,9 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname, {
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
-});
+const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
   forceWriteFileSystem: true,
 });
