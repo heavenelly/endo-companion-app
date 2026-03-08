93b9c454759e493ca98545209095beffb14b2348
cam_s <tinchantcamille@outlook.com>
2026-03-08 19:01:21 +0100

    Complete Capacitor setup for native iOS app

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+const config = {
+  appId: 'com.endocompanion.app',
+  appName: 'Endo Companion',
+  webDir: 'src',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  },
+  ios: {
+    scheme: 'EndoCompanion'
+  },
+  plugins: {}
+};
+
+export default config;
